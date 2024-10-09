34ace5d7f651e55e35e4f3da094755dd5d665982
Rishabh Sonkar <68018650+RishabhSkr@users.noreply.github.com>
2024-10-09 13:23:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Linked List Matrix/linked-list-matrix.cpp
@@ -0,0 +1,144 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *right, *down;
+
+    Node(int x) {
+        data = x;
+        right = NULL;
+        down = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+/*structure of the node of the linked list is as
+
+struct Node
+{
+    int data;
+    Node* right, *down;
+
+    Node(int x){
+        data = x;
+        right = NULL;
+        down = NULL;
+    }
+};
+*/
+
+// function must return the pointer to the first element of the in linked list i.e. that
+// should be the element at arr[0][0]
+class Solution {
+  public:
+    Node* constructLinkedMatrix(vector<vector<int>>& mat) {
+        // code Here
+        Node * dummy = new Node(-1);
+        Node * head = dummy;
+        int n = mat.size();
+        int m = mat[0].size();
+        
+        // create first list
+        for(int j =0;j<m;++j){
+            Node * newNode = new Node(mat[0][j]);
+            head->right = newNode;
+            head= newNode;
+        }
+        head= dummy->right;
+        // rest 
+        for(int i =1;i<n;++i){
+            Node * curr=NULL,*ptr = head;
+            for(int j =0;j<m;++j){
+                Node * newNode = new Node(mat[i][j]);
+                if(curr==NULL){
+                    curr= newNode;
+                }else{
+                    curr->right=newNode;
+                    curr=newNode;
+                }
+                ptr->down=newNode;
+                ptr = ptr->right;
+            }
+            head= head->down;
+        }
+        return dummy->right;
+    }
+};
+
+//{ Driver Code Starts.
+
+void display(Node* head) {
+    Node* Rp;
+    Node* Dp = head;
+    while (Dp) {
+        Rp = Dp;
+        while (Rp) {
+            cout << Rp->data << " ";
+            if (Rp->right)
+                cout << Rp->right->data << " ";
+            else
+                cout << "null ";
+            if (Rp->down)
+                cout << Rp->down->data << " ";
+            else
+                cout << "null ";
+            Rp = Rp->right;
+        }
+        Dp = Dp->down;
+    }
+}
+
+// Driver program
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // To handle new line after integer input
+
+    while (t--) {
+        string line;
+        getline(cin, line);
+        stringstream ss(line);
+        vector<int> nums;
+        int num;
+
+        // Read the first row of the matrix
+        while (ss >> num) {
+            nums.push_back(num);
+        }
+
+        int n = nums.size();
+        vector<vector<int>> mat(n, vector<int>(n));
+        mat[0] = nums;
+
+        // Read the remaining rows of the matrix
+        for (int i = 1; i < n; i++) {
+            string line;
+            getline(cin, line);
+            stringstream ss(line);
+            vector<int> nums;
+            int num;
+
+            // Read the first row of the matrix
+            while (ss >> num) {
+                nums.push_back(num);
+            }
+
+            mat[i] = nums;
+        }
+
+        Solution ob;
+        Node* head = ob.constructLinkedMatrix(mat);
+        if (!head) {
+            cout << "-1\n";
+        } else {
+            display(head);
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
