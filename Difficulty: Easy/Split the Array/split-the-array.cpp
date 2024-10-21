effbe151e806e46bc282ec1a92d853304d1fc0e2
Rishabh Sonkar <68018650+RishabhSkr@users.noreply.github.com>
2024-10-21 12:00:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Split the Array/split-the-array.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    int countgroup(vector<int>& arr) {
+        // Complete the function
+        // calculate the xor entrire array if it equal to zero then pow(2,n-1);
+        // if not zero return zero.
+        int xorVal = 0;
+        int n= arr.size();
+        for(auto x : arr)xorVal^=x;
+        return (xorVal==0)?(1<<n-1)-1:0;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        int ans = obj.countgroup(arr);
+        cout << ans << endl;
+    }
+}
+
+// } Driver Code Ends
